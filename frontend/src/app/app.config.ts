6bdf398a0ce64fdc7c82400b4111f3d23add6462
Eric Diaz <ericdiaz2003@hotmail.com>
2024-07-20 18:17:08 -0300

    Firebase added to proyect

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,7 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({ "projectId": "miportfolio-8355b", "appId": "1:463362972108:web:bb75fa4616161519556569", "storageBucket": "miportfolio-8355b.appspot.com", "apiKey": "AIzaSyBvsHCRGKLqrvJScRR0x1QPgJlUyk7XD0M", "authDomain": "miportfolio-8355b.firebaseapp.com", "messagingSenderId": "463362972108", "measurementId": "G-P3DLGDGEDD" })), provideFirestore(() => getFirestore())]
 };
